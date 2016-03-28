a1fcdbaedddcd0ca68c76f4678113ff27e235ec4
Noah Emmons <noahe99@gmail.com>
2016-03-28 11:25:04 -0400

    Changed GUI and will add functionality next.

---
+++ Twitch_Desktop/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Twitch_Desktop
+{
+    partial class twitchDesktop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStreams = new System.Windows.Forms.Label();
+            this.streamLink = new System.Windows.Forms.Button();
+            this.numOnlineL = new System.Windows.Forms.Label();
+            this.numOfUsers = new System.Windows.Forms.Label();
+            this.numOfFollowers = new System.Windows.Forms.Label();
+            this.numberOfFollowers = new System.Windows.Forms.Label();
+            this.totalViews = new System.Windows.Forms.Label();
+            this.numberOfViews = new System.Windows.Forms.Label();
+            this.favButton = new System.Windows.Forms.Button();
+            this.activeStreams = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblStreams
+            // 
+            this.lblStreams.AutoSize = true;
+            this.lblStreams.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStreams.Location = new System.Drawing.Point(11, 11);
+            this.lblStreams.Name = "lblStreams";
+            this.lblStreams.Size = new System.Drawing.Size(172, 20);
+            this.lblStreams.TabIndex = 0;
+            this.lblStreams.Text = "Current Streams/Users";
+            // 
+            // streamLink
+            // 
+            this.streamLink.Location = new System.Drawing.Point(239, 588);
+            this.streamLink.Name = "streamLink";
+            this.streamLink.Size = new System.Drawing.Size(75, 23);
+            this.streamLink.TabIndex = 4;
+            this.streamLink.Text = "Stream Link";
+            this.streamLink.UseVisualStyleBackColor = true;
+            this.streamLink.Click += new System.EventHandler(this.streamLink_Click);
+            // 
+            // numOnlineL
+            // 
+            this.numOnlineL.AutoSize = true;
+            this.numOnlineL.Location = new System.Drawing.Point(224, 38);
+            this.numOnlineL.Name = "numOnlineL";
+            this.numOnlineL.Size = new System.Drawing.Size(96, 13);
+            this.numOnlineL.TabIndex = 5;
+            this.numOnlineL.Text = "Number of Viewers";
+            // 
+            // numOfUsers
+            // 
+            this.numOfUsers.AutoSize = true;
+            this.numOfUsers.Location = new System.Drawing.Point(224, 57);
+            this.numOfUsers.Name = "numOfUsers";
+            this.numOfUsers.Size = new System.Drawing.Size(13, 13);
+            this.numOfUsers.TabIndex = 6;
+            this.numOfUsers.Text = "0";
+            // 
+            // numOfFollowers
+            // 
+            this.numOfFollowers.AutoSize = true;
+            this.numOfFollowers.Location = new System.Drawing.Point(224, 93);
+            this.numOfFollowers.Name = "numOfFollowers";
+            this.numOfFollowers.Size = new System.Drawing.Size(103, 13);
+            this.numOfFollowers.TabIndex = 7;
+            this.numOfFollowers.Text = "Number of Followers";
+            // 
+            // numberOfFollowers
+            // 
+            this.numberOfFollowers.AutoSize = true;
+            this.numberOfFollowers.Location = new System.Drawing.Point(224, 106);
+            this.numberOfFollowers.Name = "numberOfFollowers";
+            this.numberOfFollowers.Size = new System.Drawing.Size(13, 13);
+            this.numberOfFollowers.TabIndex = 8;
+            this.numberOfFollowers.Text = "0";
+            // 
+            // totalViews
+            // 
+            this.totalViews.AutoSize = true;
+            this.totalViews.Location = new System.Drawing.Point(224, 141);
+            this.totalViews.Name = "totalViews";
+            this.totalViews.Size = new System.Drawing.Size(62, 13);
+            this.totalViews.TabIndex = 9;
+            this.totalViews.Text = "Total Views";
+            // 
+            // numberOfViews
+            // 
+            this.numberOfViews.AutoSize = true;
+            this.numberOfViews.Location = new System.Drawing.Point(224, 154);
+            this.numberOfViews.Name = "numberOfViews";
+            this.numberOfViews.Size = new System.Drawing.Size(13, 13);
+            this.numberOfViews.TabIndex = 10;
+            this.numberOfViews.Text = "0";
+            // 
+            // favButton
+            // 
+            this.favButton.Location = new System.Drawing.Point(50, 588);
+            this.favButton.Name = "favButton";
+            this.favButton.Size = new System.Drawing.Size(75, 23);
+            this.favButton.TabIndex = 11;
+            this.favButton.Text = "Favorite";
+            this.favButton.UseVisualStyleBackColor = true;
+            // 
+            // activeStreams
+            // 
+            this.activeStreams.FormattingEnabled = true;
+            this.activeStreams.Location = new System.Drawing.Point(15, 38);
+            this.activeStreams.Name = "activeStreams";
+            this.activeStreams.Size = new System.Drawing.Size(168, 524);
+            this.activeStreams.TabIndex = 12;
+            // 
+            // twitchDesktop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(361, 623);
+            this.Controls.Add(this.activeStreams);
+            this.Controls.Add(this.favButton);
+            this.Controls.Add(this.numberOfViews);
+            this.Controls.Add(this.totalViews);
+            this.Controls.Add(this.numberOfFollowers);
+            this.Controls.Add(this.numOfFollowers);
+            this.Controls.Add(this.numOfUsers);
+            this.Controls.Add(this.numOnlineL);
+            this.Controls.Add(this.streamLink);
+            this.Controls.Add(this.lblStreams);
+            this.Name = "twitchDesktop";
+            this.Text = "Twitch Dekstop";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStreams;
+        private System.Windows.Forms.Button streamLink;
+        private System.Windows.Forms.Label numOnlineL;
+        private System.Windows.Forms.Label numOfUsers;
+        private System.Windows.Forms.Label numOfFollowers;
+        private System.Windows.Forms.Label numberOfFollowers;
+        private System.Windows.Forms.Label totalViews;
+        private System.Windows.Forms.Label numberOfViews;
+        private System.Windows.Forms.Button favButton;
+        private System.Windows.Forms.ListBox activeStreams;
+    }
+}
+
---
+++ Twitch_Desktop/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Twitch_Desktop
+{
+    public partial class twitchDesktop : Form
+    {
+        WebClient client;
+        bool streamsParsed = false;
+        bool updateList;
+
+        static List<String> gameName = new List<String>();
+        static List<String> displayName = new List<String>();
+        static List<String> streamerLink = new List<String>();
+
+        public twitchDesktop()
+        {
+            InitializeComponent();
+            client = new WebClient();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(new ThreadStart(LoadAPI));
+            thread.Start();
+            while (streamsParsed == false)
+            {
+                if (updateList == true)
+                {
+                    fill();
+                    streamsParsed = true;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void streamLink_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void streamList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        //Will load the pages api and json
+        void LoadAPI()
+        {
+            String gameNames = "";
+            String streamerName = "";
+            String streamLink = "";
+
+            int offset = 0;
+            int currGame = 0;
+
+            
+            //Itterates through the game titles
+            while (offset < 200)
+            {
+                var gameUrl = "https://api.twitch.tv/kraken/games/top?limit=10&offset=" + offset;
+                String jsonGame = client.DownloadString(gameUrl);
+                var jsonParsedGame = JObject.Parse(jsonGame);
+                var dataGame = jsonParsedGame["top"];
+
+                //Assigns the game names to the string
+                for (int i = 0; i < 10; i++)
+                {
+                    gameNames = gameNames + " " + dataGame[i]["game"]["name"] + " | ";
+                }
+                offset = offset + 10;
+            }
+
+
+            //Splits the games into their own name, and assigns to list after
+            String[] games = gameNames.Split('|');
+
+
+            //Formats Names to fit the websearch
+            for (int i = 0; i < games.Length; i++)
+            {
+
+                games[i] = games[i].Trim();
+
+                while (games[i].Contains(' '))
+                {
+                    games[i] = games[i].Replace(' ', '+');
+                }
+
+                Console.WriteLine(games[i]);
+            }
+
+            
+
+            //Gets streamer name
+            while (currGame < 50)
+            {
+                var streamerUrl = "https://api.twitch.tv/kraken/streams?game=" + games[currGame];
+                String jsonStreamer = client.DownloadString(streamerUrl);
+                var jsonParsedStreamer = JObject.Parse(jsonStreamer);
+                var dataStreamer = jsonParsedStreamer["streams"];
+
+                for (int i = 0; i < 30; i++)
+                {
+                    try
+                    {
+                        streamerName = streamerName + " " + dataStreamer[i]["channel"]["display_name"] + " | ";
+                        streamLink = streamLink + " " + dataStreamer[i]["channel"]["_links"]["self"] + " | ";
+                    }
+                    catch (Exception e)
+                    {
+                        
+                    }
+                }
+
+                currGame++;
+            }
+
+            //Seperates the name from string, splits, then adds it to array
+            String[] streamers = streamerName.Split('|');
+
+            //Streamer Name
+            for (int i = 0; i < streamers.Length; i++)
+            {
+                if (streamers[i].Equals(" "))
+                {
+
+                }
+                else
+                {
+                    streamers[i] = streamers[i].Trim();
+                    displayName.Add(streamers[i]);
+                }
+            }
+
+            Console.WriteLine("Streamer Names Parsed");
+
+            //Streamer Link
+            String[] link = streamLink.Split('|');
+            for (int i = 0; i < link.Length; i++)
+            {
+                if (link[i].Equals(" "))
+                {
+                    
+                }
+                else
+                {
+                    link[i] = link[i].Trim();
+                    streamerLink.Add(link[i]);
+                }
+           }
+
+            Console.WriteLine("Streamer Links Parsed");
+            updateList = true;
+
+            //Sorting lists into alphabetical order
+            displayName.Sort();
+            streamerLink.Sort();
+
+
+            Console.WriteLine("Number of streams:" + displayName.Count + " First streamer: " + displayName[0]);
+            Console.WriteLine("First link: " + streamerLink[0]);
+
+        }
+
+        void fill()
+        {
+            for (int i = 0; i < displayName.Count; i++)
+            {
+                activeStreams.Items.Add(displayName[i]);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
